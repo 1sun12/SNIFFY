2063fca32b696096d1a8fada6aeb87b56ba38020
12xsunx12 <regan_sun12@outlook.com>
2026-02-02 14:48:23 -0500

    thinking about how I wanna parse paylods

--- include/payl.h
+++ include/payl.h
@@ -1,5 +1,15 @@
 #ifndef PAYL_H
 #define PAYL_H
 
-// will code later
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct payl_t payl_t;
+struct payl_t {
+    uint8_t shift;  // ~ Payload shifter (move -1 or +1 bytes across the payload)
+    int plen;       // ~ Length of the payload in bytes
+};
+
+
 #endif
